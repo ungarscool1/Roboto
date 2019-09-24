423b5eba1695f6c4a6223c4bd8b44b579497ccd8
ungarscool1 <oel.g.28@gmail.com>
2019-09-25 01:21:10 +0200

    Add import

--- src/main/java/com/github/ungarscool1/Roboto/ServerLanguage.java
+++ src/main/java/com/github/ungarscool1/Roboto/ServerLanguage.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.Writer;
+import java.lang.Exception;
 
 import org.javacord.api.entity.server.Server;
 
