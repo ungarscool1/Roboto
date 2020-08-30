fb4e441d24d548270b132bc1fb36cde63744785c
ungarscool1 <oel.g.28@gmail.com>
2020-08-30 15:37:38 +0200

    Add somethings :)

---
+++ src/main/java/com/github/ungarscool1/Roboto/listeners/commands/utility/FunMessage.java
@@ -0,0 +1,15 @@
+package com.github.ungarscool1.Roboto.listeners.commands.utility;
+
+import org.javacord.api.entity.message.Message;
+import org.javacord.api.event.message.MessageCreateEvent;
+import org.javacord.api.listener.message.MessageCreateListener;
+
+public class FunMessage implements MessageCreateListener {
+    @Override
+    public void onMessageCreate(MessageCreateEvent event) {
+        Message message = event.getMessage();
+        if (message.getContent().equals("Make the party blob !")) {
+            message.getChannel().sendMessage("<:partyblob:423936638473994261><:partyblob:423936638473994261><:partyblob:423936638473994261>");
+        }
+    }
+}
