1f041a76cf1d3baa57607b06d9799a3541f6458a
Léo Godard <leo.godard@epitech.eu>
2021-06-06 19:43:20 +0200

    Add configuration file support

---
+++ src/main/java/com/github/ungarscool1/Roboto/Configuration.java
@@ -0,0 +1,9 @@
+package com.github.ungarscool1.Roboto;
+
+public class Configuration {
+	public String bot_token;
+	public String discord_bot_list_key;
+	public String sentry_io_dsn;
+	public boolean sentry_io_debug;
+	public double sentry_io_trace_sample_rate;
+}
