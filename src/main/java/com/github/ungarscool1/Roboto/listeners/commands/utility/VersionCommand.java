87c7b123934f42bb3f0acd793183617234af36cd
Léo Godard <leo.godard@epitech.eu>
2022-01-21 16:17:15 +0100

    Change Roboto version

--- src/main/java/com/github/ungarscool1/Roboto/listeners/commands/utility/VersionCommand.java
+++ src/main/java/com/github/ungarscool1/Roboto/listeners/commands/utility/VersionCommand.java
@@ -31,9 +31,9 @@ public class VersionCommand implements MessageCreateListener {
 			int users = api.getServers().stream().mapToInt(Server::getMemberCount).sum();
 			try {
 				embedBuilder.setTitle(language.getString("version.name"))
-						.addField("Version", "3.1.1")
+						.addField("Version", "3.1.2")
 						.addField(language.getString("version.lib.name"), language.getString("version.lib.desc"))
-						.addField("Build", "131221-20.1")
+						.addField("Build", "210122-16.2")
 						.addField("Bot owner", api.getOwner().get().getDiscriminatedName())
 						.addField(language.getString("version.github"), "https://github.com/ungarscool1/Roboto-v2")
 						.addField(language.getString("version.listen.user"), users + " " + language.getString("version.users"))
